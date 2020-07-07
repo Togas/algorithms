a0d66f8b1849c949abbb05737e123fcb50be4e38
Ben Stucke <ben-shuang-feng.stucke@adesso.de>
2020-07-07 12:07:43 +0200

    added kadanes algorithm

---
+++ kadanesAlgorithm.py
@@ -0,0 +1,19 @@
+# find longest contiguous subarray
+def kadanesAlgorithm(array):
+  left=0
+  right=0
+  bestSum=float("-inf")
+  bestArray=[]
+  current=0
+  for i, number in enumerate(array):
+    current+=number
+    if current<array[i]:
+      current=array[i]
+      left=i
+      right=i
+    else:
+      right+=1
+    if current>bestSum:
+      bestSum=current
+      bestArray=array[left:right+1]
+  return bestArray
