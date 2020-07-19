cbd87cd44454ac07c1bfa057903b6fb0a7302884
Ben Stucke <ben-shuang-feng.stucke@adesso.de>
2020-07-19 22:23:33 +0200

    added sieve of erasthoneses

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,8 @@
+
+9
+	test.java,9\4\94daecdffe4003a70f02ee8989295b32f50da2be
+:
+
+test2.java,5\a\5a23ecc4309b90824770d03ff89040d6e5cccbfa
+H
+sorting/insertionSort.py,4\f\4fa0198d3f74dd1773e35c7f9dc346716c8adfc3
---
+++ math/sieveOfEratosthenes.py
@@ -0,0 +1,14 @@
+def getPrimes(n):
+    dp=[False for i in range(n+1)]
+    for i in range(2, n):
+        current=i+i
+        while current<=n:
+            dp[current]=True
+            current+=i
+    result=[]
+    for i in range(4, len(dp)):
+        if not dp[i]:
+            result.append(i)
+    return result
+
+print(getPrimes(100)) 
